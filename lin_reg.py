fd6f3d64de203c7158ada7298e1e1b841ebcad5a
lusensama <lusensama@gmail.com>
2019-09-23 12:01:47 -0400

    Updated solution

--- lin_reg.py
+++ lin_reg.py
@@ -7,22 +7,20 @@ import tensorflow as tf
 # import tensorflow.contrib.eager as tfe
 import matplotlib.pyplot as plt
 
-tf.enable_eager_execution()
-tf.executing_eagerly()
 # Create data
 NUM_EXAMPLES = 500
 
 #define inputs and outputs with some noise 
 X = tf.random.normal([NUM_EXAMPLES])  #inputs
 noise = tf.random.normal([NUM_EXAMPLES]) #noise
-y = X * 3 + 2 + noise  #true output
+y = X * 10 + 22 + noise  #true output
 
 # Create variables.
-W = tf.Variable(15.)
-b = tf.Variable(15.)
+W = tf.Variable(0.)
+b = tf.Variable(0.)
 
 
-train_steps = 2000
+train_steps = 3000
 learning_rate = 0.1
 
 # Define the linear predictor.
@@ -36,18 +34,10 @@ def squared_loss(y, y_predicted):
 
 def huber_loss(y, y_predicted, m=0.1):
     """Huber loss."""
-<<<<<<< HEAD
     error = tf.abs(y-y_predicted)
     above = m * (error - 0.5*m)
     below = 0.5*tf.square(error)
     return tf.reduce_mean(tf.where(error<m, below, above))
-=======
-    if np.absolute(y_predicted-y) <= m:
-        return .5*((y_predicted - y)**2)
-    else:
-      return  m*np.absolute(y_predicted-y)-.5*tf.square(m)
-
->>>>>>> e2e05dd86d32b96dc39b9ea0de751ae3052b0ffd
 
 prevloss = -1.0
 # with tf.device('/cpu:0'):
@@ -56,7 +46,6 @@ for i in range(train_steps):
     with tf.GradientTape() as tape:
         # tape.watch(W)
         yhat = X * W + b
-<<<<<<< HEAD
         # loss = squared_loss(y, yhat)
         loss = huber_loss(y, yhat)
         dW, db = tape.gradient(loss, [W,b])
@@ -66,13 +55,8 @@ for i in range(train_steps):
         W.assign_sub(dW * learning_rate)
         b.assign_sub(db * learning_rate)
         prevloss = loss
-    # t1 = time.time()
-=======
-        loss = tf.reduce_mean(huber_loss(y,yhat))
-    dW, db = tape.gradient(loss, [W,b])
-    W.assign_sub(dW * learning_rate)
-    b.assign_sub(db * learning_rate)
->>>>>>> e2e05dd86d32b96dc39b9ea0de751ae3052b0ffd
+
+
     if i % 100 == 0:
         print(("Loss at step {:03d}: {:.3f}".format(i, loss)))
         # print('time cost is '+ str(t1-t0))
